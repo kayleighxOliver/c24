32ed27e3ada44af6ca3f1625d13040a1312b8516
kayleighxOliver <72995944+kayleighxOliver@users.noreply.github.com>
2020-10-25 20:57:09 -0500

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(x,y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.5,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.y=mouseY;
+      pos.x=mouseX;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      stroke("red");
+      strokeWeight(4); 
+      fill("orange");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
